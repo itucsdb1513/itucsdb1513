fa1bcd83ce30450076f27f5b709ae719ffa15702
irashyti <irashyti@itu.edu.tr>
2015-11-24 17:49:31 +0200

    Added history table. Does add and delete for history table

---
+++ history.py
@@ -0,0 +1,72 @@
+import psycopg2 as dbapi2
+from flask import render_template
+from flask import redirect
+from flask.helpers import url_for
+
+class facts:
+    def __init__(self, dsn):
+        self.dsn = dsn;
+
+    def open_page(self):
+        with dbapi2.connect(self.dsn) as connection:
+            cursor = connection.cursor()
+
+            query = """CREATE TABLE IF NOT EXISTS history (
+                        number serial PRIMARY KEY,
+                        date text NOT NULL,
+                        place text NOT NULL,
+                        fact text NOT NULL)"""
+            cursor.execute(query)
+
+            query = "SELECT * FROM history"
+            cursor.execute(query)
+            history = cursor.fetchall()
+        return render_template('history.html', history = history)
+
+    def init_table(self):
+        with dbapi2.connect(self.dsn) as connection:
+            cursor = connection.cursor()
+
+            query = """DROP TABLE IF EXISTS history"""
+            cursor.execute(query)
+
+            query = """CREATE TABLE history (
+                        number serial PRIMARY KEY,
+                        date text NOT NULL,
+                        place text NOT NULL,
+                        fact text NOT NUL)"""
+            cursor.execute(query)
+
+            query = """INSERT INTO history (date, place, fact)
+                        VALUES
+                        ('6th century AD', 'India', 'Game generated'),
+                        ('15th century', 'Europe', 'Move of pieces changed')"""
+            cursor.execute(query)
+
+            connection.commit()
+        return redirect(url_for('history'))
+
+
+
+    def addfact(self, date, place, fact):
+        with dbapi2.connect(self.dsn) as connection:
+            cursor = connection.cursor()
+
+            query = """INSERT INTO history (date, place, fact)
+                        VALUES
+                        ('%s', '%s', '%s')""" % (date, place, fact)
+            cursor.execute(query)
+
+            connection.commit()
+        return redirect(url_for('history'))
+
+
+    def deletefact(self, number):
+        with dbapi2.connect(self.dsn) as connection:
+            cursor = connection.cursor()
+
+            query = """DELETE FROM history WHERE number = '%s' """ % (number)
+            cursor.execute(query)
+
+            connection.commit()
+        return redirect(url_for('history'))
