ee78d19e67c9e7e5595085d348f1d216411047c1
irashyti <irashyti@itu.edu.tr>
2015-12-13 01:27:26 +0200

    addet tours table

--- events.py
+++ events.py
@@ -7,7 +7,7 @@ class event:
     def __init__(self, dsn):
         self.dsn = dsn;
 
-    def open_page(self):
+    def open_page(self, sort = "number"):
         with dbapi2.connect(self.dsn) as connection:
             cursor = connection.cursor()
             query = """CREATE TABLE IF NOT EXISTS events (
@@ -22,13 +22,26 @@ class event:
             query = "SELECT * FROM events"
             cursor.execute(query)
             events = cursor.fetchall()
-        return render_template('upcoming_events.html', events = events)
+
+            query = """CREATE TABLE IF NOT EXISTS tours (
+                        number serial PRIMARY KEY,
+                        cha text NOT NULL,
+                        year integer NOT NULL,
+                        players integer NOT NULL,
+                        games integer NOT NULL)"""
+            cursor.execute(query)
+
+            query = "SELECT * FROM tours"
+            cursor.execute(query)
+            tours = cursor.fetchall()
+        return render_template('upcoming_events.html', events = events, tours = tours)
 
     def init_table(self):
         with dbapi2.connect(self.dsn) as connection:
             cursor = connection.cursor()
 
-            query = """DROP TABLE IF EXISTS events"""
+            query = """DROP TABLE IF EXISTS events;
+                       DROP TABLE IF EXISTS tours"""
             cursor.execute(query)
 
             query = """CREATE TABLE events (
@@ -37,7 +50,15 @@ class event:
                         place text NOT NULL,
                         player1 text NOT NULL,
                         player2 text NOT NULL,
-                        champ text NOT NULL)"""
+                        champ text NOT NULL,
+                        UNIQUE (player1, player2));
+
+                        CREATE TABLE tours (
+                        number serial PRIMARY KEY,
+                        cha text NOT NULL,
+                        year integer NOT NULL,
+                        players integer NOT NULL,
+                        games integer NOT NULL);"""
             cursor.execute(query)
 
             query = """INSERT INTO events (date, place, player1, player2, champ)
@@ -45,7 +66,13 @@ class event:
                         ('3.November', 'Tiran', 'Ira', 'Rei', 'Albanian'),
                         ('4.December', 'Istanbul', 'Javid', 'Ahmet', 'Turkish'),
                         ('10.December', 'Ankara', 'Mursit', 'Soner', 'European'),
-                        ('15.December', 'Istanbul', 'Mehmet', 'Elif', 'World')"""
+                        ('15.December', 'Istanbul', 'Mehmet', 'Elif', 'World');
+
+                        INSERT INTO tours (cha, year, players, games)
+                        VALUES
+                        ('World', 2016, 24, 72),
+                        ('European', 2017, 16, 36),
+                        ('Asian', 2016, 16, 36);"""
             cursor.execute(query)
 
             connection.commit()
@@ -61,6 +88,14 @@ class event:
             event_up = cursor.fetchone()
         return render_template('update_event.html', event_up = event_up)
 
+    def open_updatetour(self, number):
+        with dbapi2.connect(self.dsn) as connection:
+            cursor = connection.cursor()
+            query = "SELECT * FROM tours WHERE number  = %s" % (number)
+            cursor.execute(query)
+            tour_up = cursor.fetchone()
+        return render_template('update_event.html', tour_up = tour_up)
+
     def addevent(self, date, place, player1, player2, champ):
         with dbapi2.connect(self.dsn) as connection:
             cursor = connection.cursor()
@@ -69,7 +104,17 @@ class event:
                         VALUES
                         ('%s', '%s', '%s', '%s', '%s')""" % (date, place, player1, player2, champ)
             cursor.execute(query)
+            connection.commit()
+        return redirect(url_for('upcoming_events'))
 
+    def addetour(self, cha, year, players, games):
+        with dbapi2.connect(self.dsn) as connection:
+            cursor = connection.cursor()
+
+            query = """INSERT INTO tours (cha, year, players, games)
+                        VALUES
+                        ('%s', %s, %s, %s)""" % (cha, year, players, games)
+            cursor.execute(query)
             connection.commit()
         return redirect(url_for('upcoming_events'))
 
@@ -81,6 +126,14 @@ class event:
             query = """DELETE FROM events WHERE number = '%s' """ % (number)
             cursor.execute(query)
 
+            connection.commit()
+        return redirect(url_for('upcoming_events'))
+    def deletetour(self, number):
+        with dbapi2.connect(self.dsn) as connection:
+            cursor = connection.cursor()
+
+            query = """DELETE FROM tours WHERE number = '%s' """ % (number)
+            cursor.execute(query)
             connection.commit()
         return redirect(url_for('upcoming_events'))
 
@@ -95,6 +148,15 @@ class event:
             connection.commit()
         return redirect(url_for('upcoming_events'))
 
+    def delete_tour(self, cha, year):
+        with dbapi2.connect(self.dsn) as connection:
+            cursor = connection.cursor()
+            query = """DELETE FROM tours WHERE cha = '%s'
+                        AND year = %s """ % (cha, year)
+            cursor.execute(query)
+            connection.commit()
+        return redirect(url_for('upcoming_events'))
+
     def update_event(self, number, date, place, player1, player2, champ):
         with dbapi2.connect(self.dsn) as connection:
             cursor = connection.cursor()
@@ -105,6 +167,16 @@ class event:
             cursor.execute(query)
         return redirect(url_for('upcoming_events'))
 
+    def update_tour(self, number, cha, year, players, games):
+        with dbapi2.connect(self.dsn) as connection:
+            cursor = connection.cursor()
+            query = """UPDATE tours
+                        SET cha = '%s', year = %s,
+                            player = %s, games = %s
+                        WHERE number = %s""" % (cha, year, players, games, number)
+            cursor.execute(query)
+        return redirect(url_for('upcoming_events'))
+
     def find_event(self, number):
         with dbapi2.connect(self.dsn) as connection:
             cursor = connection.cursor()
@@ -112,6 +184,14 @@ class event:
             cursor.execute(query)
             events = cursor.fetchall()
         return render_template('findevent.html', events = events)
+
+    def find_tour(self, number):
+        with dbapi2.connect(self.dsn) as connection:
+            cursor = connection.cursor()
+            query = """SELECT * FROM tours WHERE number = %s """ % (number)
+            cursor.execute(query)
+            tours = cursor.fetchall()
+        return render_template('findevent.html', tours = tours)
 
     def find_event_name(self, date, place):
         with dbapi2.connect(self.dsn) as connection:
@@ -124,6 +204,17 @@ class event:
             cursor.execute(query)
             events = cursor.fetchall()
         return render_template('findevent.html', events = events)
+
+    def find_tour_name(self, cha, date):
+        with dbapi2.connect(self.dsn) as connection:
+            cursor = connection.cursor()
+            query = """SELECT * FROM tours
+                        WHERE cha LIKE '%s%%'
+                          AND year =% s
+                        ORDER BY number """ % (cha, year)
+            cursor.execute(query)
+            tours = cursor.fetchall()
+        return render_template('findevent.html', tours = tours)
 
 
 
