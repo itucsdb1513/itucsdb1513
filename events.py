dd5d96bacf20b4c15fad87be268494e99649877d
irashyti <irashyti@itu.edu.tr>
2015-12-13 23:55:43 +0200

    try tofix initialize errors

--- events.py
+++ events.py
@@ -72,14 +72,14 @@ class event:
                         ('European', 2017, 16, 36),
                         ('Asian', 2016, 16, 36),
                         ('Albanian', 2016, 16, 36),
-                        ('Turkish', 2016, 16, 36);
+                        ('Turkish', 2016, 16, 36)
 
                         INSERT INTO events (date, place, player1, player2, champ)
                         VALUES
                         ('3.November', 'Tiran', 'Ira', 'Rei', 'Albanian'),
                         ('4.December', 'Istanbul', 'Javid', 'Ahmet', 'Turkish'),
                         ('10.December', 'Ankara', 'Mursit', 'Soner', 'European'),
-                        ('15.December', 'Istanbul', 'Mehmet', 'Elif', 'World');"""
+                        ('15.December', 'Istanbul', 'Mehmet', 'Elif', 'World')"""
             cursor.execute(query)
             connection.commit()
         return redirect(url_for('upcoming_events'))
