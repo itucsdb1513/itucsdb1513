387450489c94f039ec1dccae74357bca18fce6b9
irashyti <irashyti@itu.edu.tr>
2015-12-13 03:14:29 +0200

    .

--- server.py
+++ server.py
@@ -534,7 +534,7 @@ def upcoming_events():
         return redirect(url_for('home_page'))
 
 @app.route('/update_event/<int:key>/', methods=['GET', 'POST'])
-def update_event_page(key = None):
+def update_event(key = None):
     page = event(dsn = app.config['dsn'])
     if request.method == 'GET':
         return page.open_update_event(number = key)
